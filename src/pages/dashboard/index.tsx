aa9951c0c453b155c6ea7a24d8d35ee4fd690160
Abdulhameed <busarioyewole@gmail.com>
2024-05-16 23:47:20 +0100

    feature: Add default dashboard layout

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,8 @@
+import type { ReactElement } from "react";
+import { NextPageWithLayout } from "../_app";
+
+const Page: NextPageWithLayout = () => {
+  return <p>hello world homepage</p>;
+};
+
+export default Page;
