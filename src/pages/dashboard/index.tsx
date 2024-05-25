408553f6d769ef6fe8c2633c9c365b816c7a30b0
Abdulhameed <busarioyewole@gmail.com>
2024-05-25 21:43:36 +0100

    init compoents for dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,17 +4,12 @@ import Flex from "@/components/shared/Flex";
 import { NextPageWithLayout } from "../_app";
 import SpaceBetween from "@/components/shared/SpaceBetween";
 import { Button } from "@/components/ui/button";
-import NoSupermarkets from "@/components/views/dashboard/homepage/NoSupermarkets";
-import NoSupermarketsDialog from "@/components/views/dashboard/homepage/NoSupermarketsDialog";
-import { useDisclosure } from "@/hooks/useDisclosure";
-import { useEffect } from "react";
+
 import Router from "next/router";
+import NoSupermarkets from "@/components/dashboard/homepage/NoSupermarkets";
+import StoreItem from "@/components/dashboard/dashboard/StoreItem";
 
 const Page: NextPageWithLayout = () => {
-  const { isOpen, toggleOpenState, onOpen } = useDisclosure();
-  useEffect(() => {
-    onOpen();
-  }, []);
   return (
     <div className="h-full overflow-auto hideScroll rounded-md bg-white px-4 py-6">
       <SpaceBetween>
@@ -23,10 +18,16 @@ const Page: NextPageWithLayout = () => {
           Add Store
         </Button>
       </SpaceBetween>
-      {/* <NoSupermarketsDialog isOpen={isOpen} toggleOpenState={toggleOpenState} /> */}
+
       <Flex className="h-[90%] justify-center">
         <NoSupermarkets />
       </Flex>
+
+      <div className="mt-8 flex flex-col gap-8">
+        <StoreItem />
+        <StoreItem />
+        <StoreItem />
+      </div>
     </div>
   );
 };
