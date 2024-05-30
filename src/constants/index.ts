fce32538cc7becf043f0e76ecfcf46937eba4d9a
Abdulhameed <busarioyewole@gmail.com>
2024-05-30 15:35:12 +0100

    add constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,11 @@
 export const ADMIN_DASHBOARD_URL = "/dashboard";
+
+export const daysOfWeek = [
+  { label: "Monday", value: "Monday" },
+  { label: "Tuesday", value: "Tuesday" },
+  { label: "Wednesday", value: "Wednesday" },
+  { label: "Thursday", value: "Thursday" },
+  { label: "Friday", value: "Friday" },
+  { label: "Saturday", value: "Saturday" },
+  { label: "Sunday", value: "Sunday" },
+];
