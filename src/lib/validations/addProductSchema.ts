83c820257a53c53c29600633e5e965597c0e4bbc
kelechi <igwesiemmanuel4@gmail.com>
2024-06-05 13:49:03 +0100

    add produt design done

---
+++ src/lib/validations/addProductSchema.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const productSchema = z.object({
+  category: z.string({
+    required_error: "Please select a category.",
+  }),
+  tag: z.string({
+    required_error: "Please select a product tag.",
+  }),
+  supermarket: z.string({
+    required_error: "Please select a supermarket.",
+  }),
+  productName: z.string({
+    required_error: "Please type a name.",
+  }),
+  description: z.string().optional(),
+  price: z.string({
+    required_error: "Please type a price.",
+  }),
+  quantity: z.string({
+    required_error: "Please type a quantity.",
+  }),
+  // photo: z.string({
+  //   required_error: "Please upload a photo.",
+  // }),
+  photo: z.string().optional(),
+});
+
+export type productSchemaType = z.infer<typeof productSchema>;
