bcf7f9a2b8d5b8d49700a9cc6b16572779b18dbe
Abdulhameed <busarioyewole@gmail.com>
2024-06-06 17:25:18 +0100

    add icons

---
+++ src/components/shared/Icons.tsx
@@ -0,0 +1,15 @@
+export const CustomDropdownIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="9"
+    height="6"
+    viewBox="0 0 9 6"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M4.7315 5.39697L8.75006 1.25292C9.00082 0.994336 8.84882 0.5 8.51856 0.5H0.48144C0.15118 0.5 -0.000816405 0.994336 0.249935 1.25292L4.2685 5.39697C4.40171 5.53434 4.59829 5.53434 4.7315 5.39697Z"
+      fill="currentcolor"
+    />
+  </svg>
+);
