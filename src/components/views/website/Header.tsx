79800a4f70ff7d76fc0abb5ac61bae8d76e50ac6
Abdulhameed <busarioyewole@gmail.com>
2024-06-20 11:58:39 +0100

    add textloop to header

--- src/components/views/website/Header.tsx
+++ src/components/views/website/Header.tsx
@@ -5,21 +5,66 @@ import Router from "next/router";
 import Flex from "@/components/shared/Flex";
 import AppStoreIcon from "@/assets/icon/appstore.svg";
 import PlayStoreIcon from "@/assets/icon/playstore.svg";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+const variants = {
+  enter: () => {
+    return {
+      y: -20,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    y: 0,
+    opacity: 1,
+  },
+  exit: () => {
+    return {
+      zIndex: 0,
+      opacity: 0,
+    };
+  },
+};
+
 const Header = () => {
+  const texts = ["better", "easier"];
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      let next = index + 1;
+      if (next === texts.length) {
+        next = 0;
+      }
+      setIndex(next);
+    }, 3 * 1000);
+  }, [index, setIndex, texts.length]);
+
   return (
     <header className="w-full pt-12 lg:pt-0 items-center grid gap-4 lg:grid-cols-2">
       <div className="lg:pb-12">
-        <div>
-          <p className="text-[54px] lg:text-[66px] font-bold">
-            Grocery Shopping just got{" "}
-            <span className="text-occupy-primary">Better</span>
-          </p>
+        <p className="text-[54px] lg:text-[66px] font-bold">
+          <span>Grocery Delivery just got&nbsp; </span>
+          <AnimatePresence>
+            <motion.span
+              style={{ position: "absolute" }}
+              variants={variants}
+              key={index}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              className="text-occupy-primary"
+              transition={{
+                y: { type: "spring", stiffness: 180, damping: 10 },
+                opacity: { duration: 0.3 },
+              }}
+            >
+              {" " + texts[index]}
+            </motion.span>
+          </AnimatePresence>
+        </p>
 
-          <p className="text-[54px] lg:text-[66px] font-bold">
-            Grocery Delivery just made{" "}
-            <span className="text-occupy-primary">Easier</span>
-          </p>
-        </div>
         <p className="mb-4 text-[24px]">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
           dolorum provident nostrum sint natus, sapiente non quisquam ad
