fdc0efdc0a46aa5c757aaffacf211b5ce8c1c1e2
DeckofAces <52782439+DeckofAces@users.noreply.github.com>
2024-07-20 14:25:35 +0100

    update download app section to better match design

--- src/components/views/website/DownloadAppSection.tsx
+++ src/components/views/website/DownloadAppSection.tsx
@@ -5,9 +5,9 @@ import Container from "../../shared/Container";
 const DownloadAppSection = () => {
   return (
     <Container>
-      <section className="pb-12 flex flex-col md:flex-row lg:justify-between lg:items-center px-6 lg:px-20">
-        <div className="flex flex-col gap-4 lg:gap-12">
-          <p className="text-black font-bold text-3xl lg:text-7xl">
+      <section className="flex flex-col px-6 pb-12 md:flex-row lg:items-center lg:justify-between lg:px-20">
+        <div className="space-y-6 lg:space-y-12">
+          <p className="text-5xl font-bold leading-tight text-black lg:text-7xl">
             Download now and <br /> start shopping
           </p>
           <div className="flex flex-row gap-4">
@@ -26,7 +26,12 @@ const DownloadAppSection = () => {
           </div>
         </div>
         <div className="pt-6 lg:pt-0">
-          <Image src="/images/iphoneApp.png" alt="iphone" width={500} height={100} />
+          <Image
+            src="/images/iphoneApp.png"
+            alt="iphone"
+            width={500}
+            height={100}
+          />
         </div>
       </section>
     </Container>
--- src/subComponents/DownloadBtn.tsx
+++ src/subComponents/DownloadBtn.tsx
@@ -8,19 +8,19 @@ interface DownloadBtnProps {
   title: string;
 }
 
-const DownloadBtn: React.FC<DownloadBtnProps> = ({ href, icon, name, title }) => {
+const DownloadBtn: React.FC<DownloadBtnProps> = ({
+  href,
+  icon,
+  name,
+  title,
+}) => {
   return (
     <Link href={href}>
-      <div
-        className="
-          rounded-md cursor-pointer py-3 px-3 lg:px-0 font-medium  
-          flex items-center justify-center hover:scale-105 transform w-full md:w-[154px] bg-black
-        "
-      >
-        <img src={icon} className="h-6 mr-4" alt={`${name} icon`} />
-        <div className="flex flex-col lg:leading-[18px] text-[10px] md:text-[12px] lg:text-[14px] leading-[14px] font-bold items-left">
-          <h2 className="font-thin text-white text-opacity-75 text-xs">{title}</h2>
-          <h1 className="font-bold whitespace-nowrap text-white">{name}</h1>
+      <div className="flex w-full transform cursor-pointer items-center justify-center rounded-md bg-black px-3 py-3 font-medium hover:scale-105 md:w-[154px] lg:px-0">
+        <img src={icon} className="mr-4 h-6" alt={`${name} icon`} />
+        <div className="items-left flex flex-col text-[10px] font-bold leading-[14px] md:text-[12px] lg:text-[14px] lg:leading-[18px]">
+          <h2 className="text-xs text-[#B3B3B3]">{title}</h2>
+          <h1 className="whitespace-nowrap font-medium text-white">{name}</h1>
         </div>
       </div>
     </Link>
