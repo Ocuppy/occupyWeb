883434deb4876bc416c2bf97e179b671e56f8599
DeckofAces <52782439+DeckofAces@users.noreply.github.com>
2024-07-20 22:14:36 +0100

    - change login image background to be a proper background image - update login form responsiveness

--- src/components/onboarding/login.tsx
+++ src/components/onboarding/login.tsx
@@ -31,34 +31,37 @@ const Login = () => {
   });
 
   return (
-    <section className="lg:relative font-[inter] w-full min-h-screen flex lg:items-center lg:justify-center">
-      <div className="lg:absolute hidden lg:block top-0 left-0 w-full h-full">
-        <Image
-          src={"/images/onboarding.png"}
-          alt=""
-          layout="fill"
-          objectFit="cover"
-          className="w-full h-full"
-        />
-      </div>
-
-      <div className="lg:relative lg:z-10 flex items-center gap-20 justify-start w-full lg:py-20 lg:px-8">
-        <h1 className="text-[#EBF7FB] hidden lg:block font-bold text-5xl w-full max-w-2xl">
-          Manage your Supermarket Operations with ease using our intuitive Dashboard
+    <section
+      className="flex min-h-screen w-full font-[inter] lg:relative lg:items-center lg:justify-center"
+      style={{
+        backgroundImage: `url('/images/onboarding.png')`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="flex w-full flex-col items-center justify-start gap-20 py-20 lg:relative lg:z-10 lg:flex-row lg:px-8">
+        <h1 className="hidden w-full max-w-2xl text-5xl font-bold text-[#EBF7FB] lg:block">
+          Manage your Supermarket Operations with ease using our intuitive
+          Dashboard
         </h1>
 
         {/* login form */}
-        <div className="lg:bg-white px-6 lg:px-8 py-12 w-full max-w-lg rounded-lg lg:shadow-lg flex flex-col gap-8 items-center">
+        <div className="flex w-full max-w-lg flex-col items-center gap-8 rounded-lg bg-white px-6 py-12 lg:px-8 lg:shadow-lg">
           <Image className="w-[120px]" src={OccupyLogo} alt="logo" />
-          <div className="flex flex-col items-start gap-3 w-full">
-            <h3 className="text-[#12141A] font-medium text-2xl">Login</h3>
-            <p className="text-sm text-[#606778] font-medium pb-12">
+          <div className="flex w-full flex-col items-start gap-3">
+            <h3 className="text-2xl font-medium text-[#12141A]">Login</h3>
+            <p className="pb-12 text-sm font-medium text-[#606778]">
               Login to Access Your Account
             </p>
             {/* input form  */}
-            <form className="flex flex-col gap-4 w-full" onSubmit={formik.handleSubmit}>
-              <div className="flex flex-col gap-2 w-full">
-                <label htmlFor="email" className="font-medium text-sm text-[#606778]">
+            <form
+              className="flex w-full flex-col gap-4"
+              onSubmit={formik.handleSubmit}
+            >
+              <div className="flex w-full flex-col gap-2">
+                <label
+                  htmlFor="email"
+                  className="text-sm font-medium text-[#606778]"
+                >
                   Email
                 </label>
                 <Input
@@ -71,17 +74,22 @@ const Login = () => {
                   value={formik.values.email}
                   className={
                     formik.touched.email && formik.errors.email
-                      ? "border-red-700 w-full"
+                      ? "w-full border-red-700"
                       : "w-full"
                   }
                 />
                 {formik.touched.email && formik.errors.email ? (
-                  <div className="text-red-500 text-sm">{formik.errors.email}</div>
+                  <div className="text-sm text-red-500">
+                    {formik.errors.email}
+                  </div>
                 ) : null}
               </div>
 
-              <div className="flex flex-col gap-2 w-full relative">
-                <label htmlFor="password" className="font-medium text-sm text-[#606778]">
+              <div className="relative flex w-full flex-col gap-2">
+                <label
+                  htmlFor="password"
+                  className="text-sm font-medium text-[#606778]"
+                >
                   Password
                 </label>
                 <Input
@@ -94,46 +102,54 @@ const Login = () => {
                   value={formik.values.password}
                   className={
                     formik.touched.password && formik.errors.password
-                      ? "border-red-700 w-full"
+                      ? "w-full border-red-700"
                       : "w-full"
                   }
                 />
                 <div
-                  className="absolute top-10 right-4 cursor-pointer"
+                  className="absolute right-4 top-[2.85rem] cursor-pointer"
                   onClick={togglePasswordVisibility}
                 >
-                  {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                  {showPassword ? <EyeOff size={15} /> : <Eye size={15} />}
                 </div>
                 {formik.touched.password && formik.errors.password ? (
-                  <div className="text-red-500 text-sm">{formik.errors.password}</div>
+                  <div className="text-sm text-red-500">
+                    {formik.errors.password}
+                  </div>
                 ) : null}
               </div>
-              <article className="inline-flex gap-3 w-full">
-                <p className="font-light text-sm text-[#7B8499]">
+              <article className="inline-flex w-full gap-3">
+                <p className="text-sm font-light text-[#7B8499]">
                   Can&apos;t remember your password?{" "}
                   <Link
                     href={"/auth/forgot-password"}
-                    className="text-[#A74E8E] font-medium underline"
+                    className="font-medium text-[#A74E8E] underline"
                   >
                     Forget Password
                   </Link>
                 </p>
               </article>
 
-              <div className="text-end w-full">
-                <Button type="submit" className="w-full lg:w-32 rounded-md">
+              <div className="w-full text-end">
+                <button
+                  type="submit"
+                  className="w-full rounded-md bg-occupy-primary p-3 text-white lg:w-32"
+                >
                   Login
-                </Button>
-                <p className="text-sm text-[#7B8499] text-center lg:text-end pt-4">
+                </button>
+                <p className="pt-4 text-center text-sm text-[#7B8499] lg:text-end">
                   New to Occupy?{" "}
-                  <Link href="/auth/signup" className="text-[#A74E8E] font-medium underline">
+                  <Link
+                    href="/auth/signup"
+                    className="font-medium text-[#A74E8E] underline"
+                  >
                     Sign Up
                   </Link>
                 </p>
               </div>
             </form>
           </div>
-          <div className="flex items-center text-sm font-light text-center text-black justify-center gap-3 w-full">
+          <div className="flex w-full items-center justify-center gap-3 text-center text-sm font-light text-black">
             <Link href="#">Help</Link>
             <Link href="#">Privacy</Link>
             <Link href="#">Terms</Link>
