4eaf37c6ccbaae7eb87dc601221eb50020d5a6c5
DeckofAces <52782439+DeckofAces@users.noreply.github.com>
2025-02-09 15:53:58 +0100

    update get orders slice

--- src/store/redux/services/superMarketOrdersSlice/superMarketOrdersApiSlice.ts
+++ src/store/redux/services/superMarketOrdersSlice/superMarketOrdersApiSlice.ts
@@ -3,14 +3,13 @@ import { baseApiSlice } from "../baseApiSlice";
 export const superMarketOrdersApiSlice = baseApiSlice.injectEndpoints({
     endpoints: (build) => ({
         getSuperMarketOrders: build.query({
-            query: (supermarketId) => {
-                const queryString = new URLSearchParams(supermarketId).toString();
+            query: ({ supermarket_id, params = {} }) => {
                 return {
-                    url: `/store/supermarkets/${queryString}/received-orders`,
-                    method: "GET"
-                }
-            }
-        })
+                    url: `/store/supermarkets/${supermarket_id}/received-orders`,
+                    method: "GET",
+                };
+            },
+        }),
     }),
 });
 
