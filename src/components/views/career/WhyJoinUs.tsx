0e8a088d88871b8d6f7a5431da56ada27e225b81
vershima <kelvinityavyar@gmail.com>
2025-09-05 16:50:21 -0700

    from kelvin

--- src/components/views/career/WhyJoinUs.tsx
+++ src/components/views/career/WhyJoinUs.tsx
@@ -71,7 +71,7 @@ const jobData = [
     id: 1,
     title: "Delivery Rider",
     type: "Full Time",
-    location: "Forte Royal Estate (Airport Road)",
+    location: "Forte Royal Estate (Airport Road kiyami)",
     linkApply: "https://forms.gle/KKZ3XGfrqDW6UUvk8",
   },
   {
